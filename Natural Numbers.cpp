bad56891578f9a1945bac5b2f29dc28886ad67a7
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-06-19 21:06:03 +0530

    Create Natural Numbers.cpp

---
+++ Natural Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,i;
+    cout<<"enter a number";
+    cin>>a;
+    cout<<"the numbers are"<<endl;
+    for(i=0;i<=a;i++)
+    {
+        
+        cout<<i<<endl;
+    }
+}
