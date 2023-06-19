2d7aab814277e82f5321a89d1c3507bdfd212c8f
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-06-19 21:08:31 +0530

    Create Sum of Even Natural Numbers.cpp

---
+++ Sum of Even Natural Numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i=2;
+    int sum=0;
+    while(i<=n)
+    {
+        cout<<i<<endl;
+        sum=sum+i;
+        i=i+2;
+    }
+    cout<<"sum"<<sum;
+    return 0;
+}
