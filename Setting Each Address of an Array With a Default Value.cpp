cb0bc361f7cdec475653fda1f06061fd47be2fe9
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-06-22 11:19:59 +0530

    Create Setting Each Address of an Array With a Default Value.cpp

---
+++ Setting Each Address of an Array With a Default Value.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int i;
+    int n;
+    cin>>n;
+    int arr[100];
+    for(i=0;i<n;i++){
+    fill_n(arr, 100, 23);
+    cout<<arr[i]<<endl;;
+    }
+    
+  
+    
+    
+}
