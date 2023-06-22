50794e14f49ecf92f3a36d4b3cac4fe7d0b2f267
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-06-22 11:29:32 +0530

    Create Swapping Alternate Elements of an Array Without Swap Function.cpp

---
+++ Swapping Alternate Elements of an Array Without Swap Function.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+using namespace std;
+
+int swapalternate(int arr[], int size)
+{
+    int i;
+    int temp;
+    for(i=0;i<size;i+=2){
+        if(i+1 <size){
+            temp= arr[1];
+            arr[1]=arr[0];
+            arr[0]=temp;
+        }
+    }
+}
+void getarray(int arr[], int size)
+{
+    int i;
+    for(i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int even[6]={4,7, 9, -2, -9, 5};
+    int odd[5]={7, 4, 90, 3, 8};
+    swapalternate(even, 6);
+    getarray(even, 6);
+    swapalternate(odd, 5);
+    getarray(odd, 5);
+    return 0;
+}
+
