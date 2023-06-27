752331b5b1b5d0c9957f8f59da2737b83e32e2ed
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-06-27 21:54:09 +0530

    Update Bubble Sorting.cpp

--- Bubble Sorting.cpp
+++ Bubble Sorting.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h> 
 void bubbleSort(vector<int>& arr, int n)
-{   
+{
+    bool swapped =  false;
     for(int i=1;i<n;i++)
     {
         for(int j=0;j<n-i;j++)
         {
             if(arr[j]>arr[j+1])
             swap(arr[j], arr[j+1]);
+            swapped = true
+        }
+        if(swapped == false)
+        {
+            break;
         }
     }  
 }
