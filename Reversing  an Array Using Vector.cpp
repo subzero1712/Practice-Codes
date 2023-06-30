2d6a03df1488a01b7ca0602f2d70f2aebae5c070
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-06-30 23:56:25 +0530

    Create Reversing  an Array Using Vector.cpp

---
+++ Reversing  an Array Using Vector.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> reverse(vector<int> arr)
+{
+    int s =  0;
+    int e = arr.size() - 1;
+    while(s<=e){
+    swap(arr[s], arr[e]);
+    s++;
+    e--;
+    }
+    return arr;
+}
+void print(vector<int> arr)
+{
+    for(int i=0; i<arr.size(); i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
+int main()
+{
+    vector<int> arr;
+    arr.push_back(11);
+    arr.push_back(7);
+    arr.push_back(3);
+    arr.push_back(12);
+    arr.push_back(4);
+    vector<int> ans= reverse(arr);
+    print(ans);
+    
+    
+}R
