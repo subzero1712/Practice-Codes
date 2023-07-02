9b7eb7e995a8f65daf422ddd63b630f73ca9e314
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-07-02 10:50:53 +0530

    Update Rotate an Array After Given Index.cpp

--- Rotate an Array After Given Index.cpp
+++ Rotate an Array After Given Index.cpp
@@ -3,6 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         vector<int> temp(nums.size());
         for(int i=0; i<nums.size(); i++){
+        //humne temp variable isiliye banaya hai kyunki agar num mn store karate to kuch values overewrite ho jati aur shi answer nahi ata...
         temp[(i+k)%nums.size()] = nums[i];
         }
         nums = temp;
