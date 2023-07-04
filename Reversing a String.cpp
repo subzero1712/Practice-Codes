b410167612861704d315db18c26629fe436a00eb
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-07-04 11:48:34 +0530

    Create Reversing a String.cpp

---
+++ Reversing a String.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+void reverse(char name[], int n)
+{
+    int s=0;
+    int e=n - 1;
+    while(s<=e)
+    {
+        swap(name[s++], name[e--]);
+    }
+    
+}
+int getlength(char name[])
+{
+    int count=0;
+    for(int i=0; name[i]!='\0';i++)
+    {
+        count++;
+    }
+    return count; 
+}
+int main()
+{
+    char name[20];
+    cin>>name;
+    int len = getlength(name);
+    reverse(name, len);
+    cout<<"your reversed name is "<< endl;
+    cout<<name<<endl;
+   
+}
