79257278a5aa978767450a0ddfcb82b74a546fec
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-07-06 00:57:37 +0530

    Create Reversing Words in a String.cpp

---
+++ Reversing Words of in a String.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<string.h>
+
+
+using namespace std;
+int main()
+{
+    string s = "my name is palak";
+    int start = 0;
+    int p = 0;
+    for (int i = 0; i <= s.length(); i++)
+    {
+        if (s[p] == ' ' || s[p] == '\0')
+        {
+            int end = p - 1;
+            while (start <= end)
+            {
+                swap(s[start],s[end]);
+                start++;
+                end--;
+            }
+            start=p+1;
+        }
+        p++;
+    }
+    cout<<s;
+}
