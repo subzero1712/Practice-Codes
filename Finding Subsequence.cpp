75a57afe29b455be471a98c3e92b064fa39f024e
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2023-10-27 01:41:11 +0530

    Create Finding Subsequence.cpp

---
+++ Finding Subsequence.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+void solve(string str, string output, int index, vector<string>& ans ){
+      if(index>=str.length())
+	  {
+		  if(output.length()>0)
+		  {
+               ans.push_back(output);
+		  }
+		  
+		  return;
+	  }
+	  solve(str,output, index+1, ans);
+
+	  char str1=str[index];
+	  output.push_back(str1);
+	  solve(str, output, index+1, ans);
+
+
+}
+vector<string> subsequences(string str){
+	
+	vector<string> ans;
+	string output="";
+	int index=0;
+    solve(str, output, index, ans);
+	return ans;
+
+	
+}
