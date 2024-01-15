198ff4088741285cc8c331ac151b68c5cd849916
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-01-15 21:17:27 +0530

    Create Encapsulation Concept.cpp

---
+++ Encapsulation Concept.cpp
@@ -0,0 +1,22 @@
+class human{
+    private:
+    int health;
+    int age;
+   
+    void setHealth(int h)
+    { 
+        this->health=h;
+    }
+    void setAge(int a)
+    {
+        this->age=a;
+    }
+     int getHealth()
+    {
+        return this->health;
+    }
+    int getAge()
+    {
+        return this->age;
+    }
+};
