cf4329c3a5f2948033a9102f86a39fb8ddfd84ba
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-01-26 01:46:42 +0530

    Create Removing Loop in Linked List.cpp

---
+++ Removing Loop in Linked List.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+Node* floydCycle(node* head)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    node* slow=head;
+    node* fast=head;
+    while(slow!=NULL && fast!=NULL)
+    {
+        fast=fast->next;
+        if(fast!=NULL)
+        {
+            fast=fast->next;
+        }
+        slow=slow->next;
+        if(slow==fast)
+        {
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+node* getStart(node* head)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    node* intersection=floydCycle(head);
+    node* slow=head;
+    while(intersection!=slow)
+    {
+        intersection=intersection->next;
+        slow=slow->next;
+    }
+    return slow;
+}
+void removeLoop(node* head)
+{
+    if(head==NULL)
+    {
+        return;
+    }
+    node* startOfLoop=getStart(head);
+    node* temp=startOfLoop;
+    while(temp->next!=startOfLoop)
+    {
+        temp=temp->next;
+    }
+    temp->next=NULL;
+}
