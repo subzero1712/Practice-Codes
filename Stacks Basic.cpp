83c1b05eb9606c0a0cd9ac07ff4f9e0fb429bc54
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-02-24 02:05:41 +0530

    Update Stacks Basic.cpp

--- Stacks Basic.cpp
+++ Stacks Basic.cpp
@@ -1,3 +1,4 @@
+//in-built function
 #include <iostream>
 #include <stack>
 using namespace std;
@@ -20,6 +21,94 @@ int main()
     {
         cout<<"stack is not empty"<<endl;
     }
+
+    //creating stacks function
+#include <iostream>
+using namespace std;
+class stack{
+  public:
+  int *arr;
+  int top;
+  int size;
+  
+  stack(int size)
+  {
+      this->size = size;
+      arr = new int(size);
+      top = -1;
+  }
+  
+  void push(int element)
+  {
+    if(size-top>1)  
+    {
+        top++;
+        arr[top]=element;
+    }
+    else
+    {
+        cout<<"stack overflow "<<endl;
+    }
+  }
+  void pop()
+  {
+      if(top>=0)
+      {
+        top--;
+      }
+      else
+      {
+        cout<<"stack empty "<<endl;
+      }
+  }
+  
+  int peak()
+  {
+      if(top>=0)
+      {
+        return arr[top];
+      }
+      else
+      {
+          cout<<"empty stack "<<endl;
+      }
+  }
+  
+  bool isempty()
+  {
+      if(top==-1)
+      {
+          return true;
+      }
+      else
+      {
+          return false;
+      }
+  }
+};
+
+int main()
+{
+    stack st(5);
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+    cout<<st.peak()<<endl;
+    st.pop();
+    cout<<st.peak()<<endl;
+    st.pop();
+    cout<<st.isempty()<<endl;
+    cout<<st.peak()<<endl;
+    st.pop();
+    st.peak();
+    st.pop();
+    cout<<st.isempty();
+    
+    
+}
       cout<<"size of stack "<<s.size()<<endl;
 
 }
