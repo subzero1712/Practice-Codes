7973b93d1cb588c5e4bc8e82014a69bf17ff07e8
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-03-07 00:25:16 +0530

    Update Searching  Element in Singly Linked List.cpp

--- Searching  Element in Singly Linked List.cpp
+++ Searching  Element in Singly Linked List.cpp
@@ -1,1 +1,18 @@
-                temp=temp->next;
+      class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, struct Node* head, int key) {
+        Node* temp=head;
+        bool flag;
+        while(temp!=NULL)
+        {
+            if(temp->data==key)
+            {
+                return true;
+            }
+            temp=temp->next;
+        
+        }
+    }
+};
+
