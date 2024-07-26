5e246994f259a760f00e1d483b4e39fdb36ac5d5
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-07-26 23:59:30 +0530

    Create Diameter of a Binary Tree.cpp

---
+++ Diameter of a Binary Tree.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  private:
+    int height(struct Node* node){
+        if(node==NULL){
+            return 0;
+        }
+        
+        int right = height(node->right);
+        int left = height(node->left);
+        
+        int ans = max(left, right)+1;
+        return ans;
+    }
+  public:
+    // Function to return the diameter of a Binary Tree.
+    pair<int,int> diameterFast(Node* root){
+         if(root==NULL){
+             pair<int,int> p = make_pair(0,0);
+            return p;
+        }
+        
+        pair<int,int> left = diameterFast(root->left);
+        pair<int,int> right = diameterFast(root->right);
+        
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second+right.second+1;
+        
+        pair<int,int> ans;
+        ans.first = max(op1,max(op2,op3));
+        ans.second = max(right.second, left.second)+1;
+        
+        return ans;
+        
+    }
+    int diameter(Node* root) {
+        // Your code here
+       return diameterFast(root).first;
+        
+    }
+};
