01d87b3dcf80035809d2b66c2cf366ab0cfa7dc3
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-07-31 11:46:22 +0530

    Create Right View of Binary tree.cpp

---
+++ Right View of Binary tree.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    private:
+    void solve(Node* root, vector<int> &ans, int level){
+        
+        if(root==NULL){
+            return;
+        }
+        if(level == ans.size()){
+            ans.push_back(root->data);
+        }
+        solve(root->right, ans, level+1);
+        solve(root->left, ans, level+1);
+    }
+    //Function to return list containing elements of right view of binary tree.
+    public:
+    vector<int> rightView(Node *root)
+    {
+       // Your Code here
+        vector<int> ans;
+        solve(root, ans, 0);
+        return ans;
+      
+    }
+};
