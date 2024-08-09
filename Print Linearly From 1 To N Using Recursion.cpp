a81e1e034b658ed5bfe060d754ea28e8809a1a40
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-08-09 10:46:06 +0530

    Create Print Linearly From 1 To N Using Recursion.cpp

---
+++ Print Linearly From 1 To N Using Recursion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void print(int i,int n){
+    if(i>n){
+        return;
+    }
+    cout<<i<<endl;
+    print(i+1,5);
+}
+int main()
+{
+    int i=1;
+    int m=5;
+    print(i,m);
+}
