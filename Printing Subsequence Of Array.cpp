eb492b0e00824a1465e58cc68bf89e98aef22cd1
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-08-13 10:46:07 +0530

    Create Printing Subsequence Of Array.cpp

---
+++ Printing Subsequence Of Array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void print(int ind, vector<int> &ds, int arr[], int n){
+    if(ind==n){
+        for(auto it : ds){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    ds.push_back(arr[ind]);
+    print(ind+1, ds, arr, n);
+    ds.pop_back();
+    print(ind+1, ds, arr, n);
+}
+int main()
+{
+    int arr[] = {3,1,2};
+    int n=3;
+    vector<int> ds;
+    print(0,ds,arr,n);
+    return 0;
+}
