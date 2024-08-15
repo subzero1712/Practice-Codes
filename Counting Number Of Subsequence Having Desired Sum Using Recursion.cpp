8ff7414f1a820e4444ba761b08e9fa360ca547d5
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-08-15 11:23:04 +0530

    Create Counting Number Of Subsequence Having Desired Sum Using Recursion.cpp

---
+++ Counting Number Of Subsequence Having Desired Sum Using Recursion.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int printS(int ind, int s, int sum, int arr[], int n){
+  //condition not satisfied
+  //strictly done if array contains positive numbers
+  if(s>sum) return 0;
+  if(ind==n){
+    if(s==sum){
+      return 1;
+    }
+    else return 0;
+  }
+  
+  s+=arr[ind];
+  int l = printS(ind+1,s,sum,arr,n);
+  s-=arr[ind];
+  int r = printS(ind+1,s,sum,arr,n);
+
+  return l + r;
+
+}
+int main(){
+  int arr[] = {1,2,1,1};
+  int n = 4;
+  int sum = 2;
+  cout<<printS(0,0,sum,arr,n);
+  return 0;
+}
