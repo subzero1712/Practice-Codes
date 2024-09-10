6d749e5c8b96b5ae6bcae53d5a5f3ff1f714bfbf
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-09-10 12:12:44 +0530

    Update Rotate Array By K Places.cpp

--- Rotate Array By K Places.cpp
+++ Rotate Array By K Places.cpp
@@ -14,3 +14,23 @@ vector<int> rotateArray(vector<int>arr, int k) {
     }
     return arr;
 }
+
+//2nd Approach
+void reverse(vector<int> &arr, int start, int end){
+    while(start<=end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
+vector<int> rotateArray(vector<int>arr, int k) {
+    // Write your code here.
+    int n = arr.size();
+    k=k%n;
+    reverse(arr, 0, k-1);
+    reverse(arr, k, n-1);
+    reverse(arr, 0, n-1);
+    return arr;
+}
