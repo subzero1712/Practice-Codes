dff56cc7f448acd70a46e7c0808c8d1cc33eb1e7
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-09-11 12:37:59 +0530

    Create Union Of Sorted Array Optimal Approach.cpp

---
+++ Union Of Sorted Array Optimal Approach.cpp
@@ -0,0 +1,37 @@
+vector < int > sortedArray(vector < int > a, vector < int > b) {
+    int n1=a.size();
+    int n2=b.size();
+    int i=0;
+    int j=0;
+    vector<int> unionArr;
+    while(i<n1 && j<n2){
+        if(a[i]<=b[j]){
+            //only works for sorted array
+            if(unionArr.size()==0 || unionArr.back() != a[i]){
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else{
+            if(unionArr.size()==0 || unionArr.back() != b[j]){
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+    while(j<n2){
+        if(unionArr.size()==0 || unionArr.back() != b[j]){
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+
+    while(i<n1){
+        if(unionArr.size()==0 || unionArr.back() != a[i]){
+                unionArr.push_back(a[i]);
+            }
+            i++;
+
+    }
+    return unionArr;
+}
