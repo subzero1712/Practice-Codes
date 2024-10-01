4e14674f509b789bd7ab4b6218b3fd375d572a7d
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-10-02 01:59:13 +0530

    Update Rotate Matrix 90 Degree.cpp

--- Rotate Matrix 90 Degree.cpp
+++ Rotate Matrix 90 Degree.cpp
@@ -20,4 +20,22 @@ public:
         
     }
 };
+
+//OR
+
+#include<vector>
+
+void rotateMatrix(vector<vector<int>> &mat){
+	int n=mat.size();
+	//transpose of a matrix
+	for(int i=0;i<n-1;i++){
+		for(int j=i+1;j<n;j++){
+			swap(mat[i][j], mat[j][i]);
+		}
+	}
+	//reversing a matrix after transposing
+	for(int i=0;i<n;i++){
+		reverse(mat[i].begin(),mat[i].end());
+	}
+}
         
