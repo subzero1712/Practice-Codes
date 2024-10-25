49ad7c318d4c2c6f5ddb995c5a2a3146895922bf
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2024-10-25 12:58:20 +0530

    Update Merge All Overlapping Intervals.cpp

--- Merge All Overlapping Intervals.cpp
+++ Merge All Overlapping Intervals.cpp
@@ -24,3 +24,22 @@ vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
 	return ans;
 	
 }
+
+//Optimal Approach
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }
+    }
+    return ans;
+    }
+};
