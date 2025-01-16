1b4824ca6048ae5084045142128546b787359b78
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-01-16 23:25:19 +0530

    Update First And Last Occurrence Of a Number Using Binary Search.cpp

--- First And Last Occurrence Of a Number Using Binary Search.cpp
+++ First And Last Occurrence Of a Number Using Binary Search.cpp
@@ -1,3 +1,4 @@
+//using lowerbound and upperbound
 int lowerBound(const vector<int>& arr, int n, int x) {  
     int low = 0;     
     int high = n - 1;     
@@ -36,4 +37,48 @@ pair<int, int> firstAndLastPosition(const vector<int>& arr, int n, int k) {
         return {-1, -1};     
     }     
     return {lb, upperBound(arr, n, k) - 1}; 
+}
+
+//using direct binary search
+int first(vector<int> &arr, int n, int k){
+    int low=0;
+    int high=n-1;
+    int ans=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]==k){
+            ans=mid;
+            high=mid-1;
+        }
+        else if(arr[mid]<k){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return ans;
+}
+int last(vector<int> &arr, int n, int k){
+    int low=0;
+    int high=n-1;
+    int ans=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]==k){
+            ans=mid;
+            low=mid+1;
+        }
+        else if(arr[mid]<k){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return ans;
+}
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    return {first(arr,n,k),last(arr,n,k)};
 }
