63efffc600988ace146de07d23027df766bec947
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-01-18 01:45:48 +0530

    Update Finding Peak Element in a Mountain Array.cpp

--- Finding Peak Element in a Mountain Array.cpp
+++ Finding Peak Element in a Mountain Array.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-      int s=0;
-      int e= arr.size() - 1;
-      int mid = s + (e-s)/2;
-      while(s<e)
-      {
-          if(arr[mid]<arr[mid + 1]){
-              s= mid + 1;
-          }
-          else{
-              e = mid;
+    int findPeakElement(vector<int>& arr) {
+    int n=arr.size();
+    int low=1;
+    int high=n-2;
+    if(n==1) return 0;
+    if(arr[0]>arr[1]){
+        return 0;
+    } 
+    if(arr[n-2]<arr[n-1]){
+        return n-1;
+    }
 
-          }
-           mid = s + (e-s)/2;
-      } 
-      return s;  
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+            return mid;
+        }
+        else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return -1;
     }
 };
