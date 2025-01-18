82068ab8fee6916972941ace4caf8d3fc6e314bd
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-01-18 23:31:44 +0530

    Create Finding Floor Value of Square Root of a Number Using Binary Search.cpp

---
+++ Finding Floor Value of Square Root of a Number Using Binary Search.cpp
@@ -0,0 +1,18 @@
+int floorSqrt(int n)
+{
+    int low=1;
+    int high=n;
+    int ans=1;
+    while(low<=high){
+        long long mid=low+(high-low)/2;
+        long long val=mid*mid;
+        if(val<=n){
+            ans=mid;
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return high;
+}
