d6382101a73862124c119b711b0fd25ff188b894
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-01-25 01:52:50 +0530

    Update DFS Pre-Order In-Order Post-Order Tree Traversals.cpp

--- DFS Pre-Order In-Order Post-Order Tree Traversals.cpp
+++ DFS Pre-Order In-Order Post-Order Tree Traversals.cpp
@@ -36,3 +36,47 @@ vector<vector<int>> getTreeTraversal(TreeNode *root){
     ans.push_back(pos);
     return ans;
 }
+//all at once using one stack
+class Solution {
+public:
+    vector<int> postOrder(Node* root) {
+        // Use a stack of pairs to track the node and its state
+        stack<pair<Node*, int>> st;
+        st.push({root, 1});
+        vector<int> pre, in, post;
+
+        if (root == NULL) return post; // Return empty vector if the root is NULL
+
+        while (!st.empty()) {
+            auto it = st.top();
+            st.pop();
+
+            // Preorder: State 1
+            if (it.second == 1) {
+                pre.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+
+                if (it.first->left != NULL) {
+                    st.push({it.first->left, 1});
+                }
+            }
+            // Inorder: State 2
+            else if (it.second == 2) {
+                in.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+
+                if (it.first->right != NULL) {
+                    st.push({it.first->right, 1});
+                }
+            }
+            // Postorder: State 3
+            else {
+                post.push_back(it.first->val);
+            }
+        }
+
+        return post; // Return the postorder traversal
+    }
+};
