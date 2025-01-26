ec43e402f7fbb0dcb4402e39f52d08ff030b834b
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-01-26 11:57:13 +0530

    Update Check If Tree is Balanced.cpp

--- Check If Tree is Balanced.cpp
+++ Check If Tree is Balanced.cpp
@@ -1,36 +1,19 @@
-class Solution{
-     
-    public:
-    //Function to check whether a binary tree is balanced or not.
-    pair<int,int> isBalancedFast(Node* root){
-         if(root==NULL){
-            pair<bool,int> p = make_pair(true, 0); 
-            return p;
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(dfs(root)==-1){
+            return false;
         }
-        
-        pair<int,int> left = isBalancedFast(root->left);
-        pair<int,int> right = isBalancedFast(root->right);
-        
-        bool leftAns = left.first;
-        bool rightAns = right.first;
-        
-        bool diff = abs(left.second-right.second)<=1;
-        
-        pair<bool,int> ans;
-        ans.second = max(left.second,right.second)+1;
-        
-        if(leftAns && rightAns && diff){
-            ans.first = 1;
-        }
-        else{
-            ans.first = false;
-        }
-        return ans;
-        
+        return true;
     }
-    bool isBalanced(Node *root)
-    {
-        return isBalancedFast(root).first;
-       
+    int dfs(TreeNode* root){
+        if(root==NULL) return 0;
+        int l=dfs(root->left);
+        if(l==-1) return -1;
+        int r=dfs(root->right);
+        if(r==-1) return -1;
+
+        if(abs(l-r)>1) return -1;
+        return max(l,r)+1;
     }
 };
