6bba0b309809ae9187c07f4927aa3fd7fccda300
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-01-26 12:50:53 +0530

    Update Diameter of a Binary Tree.cpp

--- Diameter of a Binary Tree.cpp
+++ Diameter of a Binary Tree.cpp
@@ -1,41 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-  private:
-    int height(struct Node* node){
-        if(node==NULL){
-            return 0;
-        }
-        
-        int right = height(node->right);
-        int left = height(node->left);
-        
-        int ans = max(left, right)+1;
-        return ans;
+public:
+    int maxDepth(TreeNode* root,int& maxi) {
+        if(root==NULL) return 0;
+        int l=maxDepth(root->left,maxi);
+        int r=maxDepth(root->right,maxi);
+        maxi=max(maxi,l+r);
+        return 1+max(l,r);
     }
-  public:
-    // Function to return the diameter of a Binary Tree.
-    pair<int,int> diameterFast(Node* root){
-         if(root==NULL){
-             pair<int,int> p = make_pair(0,0);
-            return p;
-        }
-        
-        pair<int,int> left = diameterFast(root->left);
-        pair<int,int> right = diameterFast(root->right);
-        
-        int op1 = left.first;
-        int op2 = right.first;
-        int op3 = left.second+right.second+1;
-        
-        pair<int,int> ans;
-        ans.first = max(op1,max(op2,op3));
-        ans.second = max(right.second, left.second)+1;
-        
-        return ans;
-        
-    }
-    int diameter(Node* root) {
-        // Your code here
-       return diameterFast(root).first;
-        
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter=0;
+        maxDepth(root,diameter);
+        return diameter;
     }
 };
