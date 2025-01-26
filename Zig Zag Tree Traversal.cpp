97f7370691fbf2d22410c405d516ddedeafc5957
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-01-26 21:36:03 +0530

    Update Zig Zag Tree Traversal.cpp

--- Zig Zag Tree Traversal.cpp
+++ Zig Zag Tree Traversal.cpp
@@ -1,41 +1,30 @@
-class Solution{
-    public:
-    //Function to store the zig zag order traversal of tree in a list.
-    vector <int> zigZagTraversal(Node* root)
-    {
-    	vector<int> result;
-    	if(root==NULL){
-    	    return result;
-    	}
-    	queue<Node*> q;
-    	q.push(root);
-    	
-    	bool LeftToRight=true;
-    	
-    	while(!q.empty()){
-    	    int size = q.size();
-    	    vector<int> ans(size);
-    	    for(int i=0;i<size;i++){
-    	        
-    	        Node* frontnode = q.front();
-    	        q.pop();
-    	        
-    	        int index = LeftToRight? i:size-i-1;
-    	        ans[index]=frontnode->data;
-    	        
-    	        if(frontnode->left){
-    	            q.push(frontnode->left);
-    	        }
-    	        
-    	         if(frontnode->right){
-    	            q.push(frontnode->right);
-    	        }
-    	    }
-    	    LeftToRight = !LeftToRight;
-    	    for(auto i: ans){
-    	        result.push_back(i);
-    	    }
-    	}
-    	return result;
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if(root==NULL) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool lefttoright=1;
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> row(size);
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                int index=lefttoright?i:(size-i-1);
+                row[index]=node->val;
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+            lefttoright=!lefttoright;
+            result.push_back(row);
+        }
+        return result;
     }
 };
