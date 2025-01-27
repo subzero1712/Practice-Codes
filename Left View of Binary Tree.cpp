44a1a027025fc09a5cd93324491fdb5b95317729
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-01-27 21:08:25 +0530

    Update Left View of Binary Tree.cpp

--- Left View of Binary Tree.cpp
+++ Left View of Binary Tree.cpp
@@ -1,19 +1,18 @@
-void solve(Node* root, vector<int> &ans, int level){
-        
+class Solution {
+public:
+    void func1(TreeNode* root, int level,vector<int> &ds){
         if(root==NULL){
             return;
         }
-        if(level == ans.size()){
-            ans.push_back(root->data);
+        if(level==ds.size()){
+            ds.push_back(root->val);
         }
-        solve(root->left, ans, level+1);
-        solve(root->right, ans, level+1);
+        func1(root->right,level+1,ds);
+        func1(root->left,level+1,ds);
     }
-//Function to return a list containing elements of left view of the binary tree.
-vector<int> leftView(Node *root)
-{
-   // Your code here
-    vector<int> ans;
-    solve(root, ans, 0);
-    return ans;
-}
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ds;
+        func1(root,0,ds);
+        return ds;
+    }
+};
