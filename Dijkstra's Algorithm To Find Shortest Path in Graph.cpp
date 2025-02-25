543da828b39f9f6df9bde90b7d3db0c74cc8b982
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-02-26 01:20:04 +0530

    Update Dijkstra's Algorithm To Find Shortest Path in Graph.cpp

--- Dijkstra's Algorithm To Find Shortest Path in Graph.cpp
+++ Dijkstra's Algorithm To Find Shortest Path in Graph.cpp
@@ -1,3 +1,4 @@
+//Using Priority Queue
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -24,6 +25,43 @@ vector<int> dijkstra(vector<vector<int>> &vec, int v, int e, int src) {
             if (dis + edgeWeight < dist[adjNode]) {
                 dist[adjNode] = dis + edgeWeight;
                 pq.push({dist[adjNode], adjNode});
+            }
+        }
+    }
+    return dist;
+}
+
+//Using  Set DS
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> dijkstra(vector<vector<int>> &vec, int v, int e, int src) {
+    set<pair<int,int>> st;
+    vector<int> dist(v, INT_MAX);
+    dist[src] = 0;
+    st.insert({0,src});
+    vector<vector<pair<int, int>>> adj(v);
+    for (auto &edge : vec) {
+        adj[edge[0]].push_back({edge[1], edge[2]});
+        adj[edge[1]].push_back({edge[0], edge[2]}); 
+    }
+
+    while (!st.empty()) {
+        auto it=*(st.begin());
+        int node = it.second;
+        int dis = it.first;
+        st.erase(it);
+
+        for (auto it : adj[node]) {
+            int adjNode = it.first;
+            int edgeWeight = it.second;
+
+            if (dis + edgeWeight < dist[adjNode]) {
+                if(dist[adjNode]!=INT_MAX){
+                    st.erase({dist[adjNode],adjNode});
+                }
+                dist[adjNode] = dis + edgeWeight;
+                st.insert({dist[adjNode], adjNode});
             }
         }
     }
