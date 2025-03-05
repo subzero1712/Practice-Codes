ac216b30409e12bf05276469a08db64440ecb98a
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-03-06 01:43:06 +0530

    Create Prims's Algorithm For Minimum Spanning Tree.cpp

---
+++ Prims's Algorithm For Minimum Spanning Tree.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int spanningTree(int v, vector<vector<int>> adj[]) {
+       priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+       vector<int> vis(v,0);
+       pq.push({0,0});
+       int sum=0;
+       while(!pq.empty()){
+           auto it=pq.top();
+           pq.pop();
+           int node=it.second;
+           int wt=it.first;
+           if(vis[node]==1)continue;
+           vis[node]=1;
+           sum+=wt;
+           for(auto it:adj[node]){
+               int adjnode=it[0];
+               int edw=it[1];
+               if(!vis[adjnode]){
+                   pq.push({edw,adjnode});
+               }
+           }
+       }
+       return sum;
+    }
+};
