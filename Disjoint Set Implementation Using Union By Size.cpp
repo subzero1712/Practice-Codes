e57bdedc9c3477c052249932c8d05652c1eb721b
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-03-06 21:57:04 +0530

    Update Disjoint Set Implementation Using Union By Size.cpp

--- Disjoint Set Implementation Using Union By Size.cpp
+++ Disjoint Set Implementation Using Union By Size.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 class DisjointSet {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
 public:
     DisjointSet(int n) {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
+        size.resize(n + 1);
         for (int i = 0; i <= n; i++) {
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
@@ -30,6 +32,20 @@ public:
         else {
             parent[ulp_v] = ulp_u;
             rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
     }
 };
