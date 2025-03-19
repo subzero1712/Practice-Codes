f2a61a2aa09794be20cc2e8bef2c7521f69e34c1
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-03-20 01:08:09 +0530

    Update Number Of Provinces in Graph.cpp

--- Number Of Provinces in Graph.cpp
+++ Number Of Provinces in Graph.cpp
@@ -1,3 +1,4 @@
+//using dfs traversal
 class Solution {
 private:
     void dfs(vector<vector<int>>& adj, vector<int>& vis, int node) {
@@ -33,6 +34,62 @@ public:
             }
         }
 
+        return cnt;
+    }
+};
+
+//using kruskal's algorithm
+class DisjointSet {
+    vector<int> rank, parent;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int v=isConnected.size();
+        DisjointSet ds(v);
+        for(int i=0;i<v;i++){
+            for(int j=0;j<v;j++){
+                if(isConnected[i][j]==1){
+                    ds.unionByRank(i,j);
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<v;i++){
+            if(ds.findUPar(i)==i){
+                cnt++;
+            }
+        }
         return cnt;
     }
 };
