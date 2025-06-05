b0d8f60429c9de8681629ab004b5a3ea1cb4986d
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-06-05 13:19:02 +0530

    Update Frog Jump Using DP.cpp

--- Frog Jump Using DP.cpp
+++ Frog Jump Using DP.cpp
@@ -32,3 +32,22 @@ int frogJump(int n, vector<int> &heights)
     }
     return dp[n-1];
 }
+
+//Space Optimization
+#include <bits/stdc++.h> 
+int frogJump(int n, vector<int> &heights)
+{
+    int prev=0;
+    int prev2=0;
+    for(int i=1;i<n;i++){
+        int fs=prev+abs(heights[i]-heights[i-1]);
+        int ss=INT_MAX;
+        if(i>1){
+            ss=prev2+abs(heights[i]-heights[i-2]);
+        }
+        int curri=min(fs,ss);
+        prev2=prev;
+        prev=curri;
+    }
+    return prev;
+}
