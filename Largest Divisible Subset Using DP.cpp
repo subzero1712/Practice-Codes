6e90ae73b75abbc36245d7bbce0b4b7c4378f5c7
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-07-01 10:25:56 +0530

    Create Largest Divisible Subset Using DP.cpp

---
+++ Largest Divisible Subset Using DP.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n, 1), hash(n);
+        int maxi = 1, lastIndex = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
+            hash[i] = i; 
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && 1 + dp[j] > dp[i]) {
+                    dp[i] = 1 + dp[j];
+                    hash[i] = j; 
+                }
+            }
+            if (dp[i] > maxi) {
+                maxi = dp[i];
+                lastIndex = i;
+            }
+        }
+
+        vector<int> temp;
+        temp.push_back(nums[lastIndex]);
+        while (hash[lastIndex] != lastIndex) {
+            lastIndex = hash[lastIndex];
+            temp.push_back(nums[lastIndex]);
+        }
+        reverse(temp.begin(), temp.end());
+        return temp;
+    }
+};
