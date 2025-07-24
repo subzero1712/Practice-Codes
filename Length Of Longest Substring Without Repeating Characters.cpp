55e31a0471b311e5ecd2f82cd09e32f9e7c03e23
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-07-24 19:22:42 +0530

    Create Length Of Longest Substring Without Repeating Characters.cpp

---
+++ Length Of Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxlen=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            int hash[256]={0};
+            for(int j=i;j<n;j++){
+                if(hash[s[j]]==1) break;
+                else{
+                    int len=j-i+1;
+                    maxlen=max(maxlen,len);
+                    hash[s[j]]=1;
+                }
+            }
+        }
+        return maxlen;
+    }
+};
