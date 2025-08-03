276454ba008f8ce58e71dbd6666a5d2d3260f3f0
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-08-03 13:12:49 +0530

    Update Remove Duplicates And Return Size Of New Array.cpp

--- Remove Duplicates And Return Size Of New Array.cpp
+++ Remove Duplicates And Return Size Of New Array.cpp
@@ -1,3 +1,20 @@
+//1st Way
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int index = 1;
+        int n = nums.size();
+        for(int i = 1; i < n; i++){
+            if(nums[i] != nums[i - 1]){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
+
+//2nd Way
 int removeDuplicates(vector<int> &arr, int n) {
 	// Write your code here.
 	int i=0;
