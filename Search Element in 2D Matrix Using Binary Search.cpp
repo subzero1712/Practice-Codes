9a002f37794c2226e1fcfdebcbc8ec92efc32988
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-09-12 19:24:23 +0530

    Update Search Element in 2D Matrix Using Binary Search.cpp

--- Search Element in 2D Matrix Using Binary Search.cpp
+++ Search Element in 2D Matrix Using Binary Search.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = matrix.size();
-        int col = matrix[0].size();
-        int s = 0;
-        int e = row*col - 1;
-        int mid = s + (e - s)/2;
-        while(s<=e)
-        {
-            int element = matrix[mid/col][mid%col];
-            if(element==target)
-            {
-                return 1;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            if(target>=matrix[i][0] && target<=matrix[i][m-1]){
+                int low=0;
+                int high=m-1;
+                while(low<=high){
+                    int mid=low+(high-low)/2;
+                    if(matrix[i][mid]==target) return true;
+                    else if(matrix[i][mid]<target) low=mid+1;
+                    else high=mid-1;
+                }
             }
-            if(element<target)
-            {
-                s = mid + 1;
-            }
-            else
-            {
-                e = mid - 1;
-            }
-            mid = s + (e - s)/2;
         }
-        return 0;
-        
+        return false;
+    }
+};
+
+//Optimal Approach
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int low=0;
+        int high=(n*m)-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return false;
     }
 };
