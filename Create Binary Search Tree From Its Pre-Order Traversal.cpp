44a431a1fcdb0f727cb20819c1d6d5f2dadfe705
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-10-04 00:15:26 +0530

    Update Create Binary Search Tree From Its Pre-Order Traversal.cpp

--- Create Binary Search Tree From Its Pre-Order Traversal.cpp
+++ Create Binary Search Tree From Its Pre-Order Traversal.cpp
@@ -1,28 +1,27 @@
-/*************************************************************
-
-    Following is the Binary Tree node structure
-    class TreeNode
-    {
-    public:
-        int data;
-        TreeNode *left, *right;
-        TreeNode() : data(0), left(NULL), right(NULL) {}
-        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
-        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
-    };
-*************************************************************/
-TreeNode* create(vector<int>& preOrder, int &i, int bound){
-    if(i==preOrder.size() || preOrder[i]>=bound){
-        return NULL;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* build(vector<int>& preorder, int& i, int bound){
+        if(i==preorder.size() || preorder[i]>bound) return NULL;
+        TreeNode* root=new TreeNode(preorder[i++]);
+        //upper bound of the node is used
+        root->left=build(preorder,i,root->val);
+        //upper bound of parent is used
+        root->right=build(preorder,i,bound);
+        return root;
     }
-    TreeNode* root=new TreeNode(preOrder[i++]);
-    root->left=create(preOrder,i,root->data);
-    root->right=create(preOrder,i,bound);
-    return root;
-}
-
-TreeNode *preOrderTree(vector<int> &preOrder)
-{
-    int i=0;
-    return create(preOrder, i, INT_MAX);
-}
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i=0;
+        return build(preorder,i,INT_MAX);
+    }
+};
