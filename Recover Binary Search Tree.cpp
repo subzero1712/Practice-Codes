75f0c0898a546cc26bdca8cfdbe7a9af02f8f0fc
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-10-05 22:41:08 +0530

    Update Recover Binary Search Tree.cpp

--- Recover Binary Search Tree.cpp
+++ Recover Binary Search Tree.cpp
@@ -10,32 +10,33 @@
  * };
  */
 class Solution {
-private:
 TreeNode* prev;
 TreeNode* first;
 TreeNode* middle;
 TreeNode* last;
-private:
-void inorder(TreeNode* root){
-    if(root==NULL) return;
-    inorder(root->left);
-    if(prev!=NULL && root->val<prev->val){
-        if(first==NULL){
-            first=prev;
-            middle=root;
-        }else{
-            last=root;
-        }
-    }
-    prev=root;
-    inorder(root->right);
-}
 public:
+    void inorder(TreeNode* root){
+        if(root==NULL) return;
+        inorder(root->left);
+        if(prev!=NULL && root->val<prev->val){
+            //first violation
+            if(first==NULL){
+                first=prev;
+                middle=root;
+            }
+            else{
+                //second violation
+                last=root;
+            }
+        }
+        prev=root;
+        inorder(root->right);
+    }
     void recoverTree(TreeNode* root) {
-        first=middle=last=NULL;
+        first=last=middle=NULL;
         prev=new TreeNode(INT_MIN);
         inorder(root);
         if(first && last) swap(first->val,last->val);
-        else if(first && middle) swap(first->val,middle->val);
+        else if(first && middle) swap(first->val, middle->val);
     }
 };
