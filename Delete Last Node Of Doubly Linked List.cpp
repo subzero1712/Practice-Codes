f23e8235c75a33e38b03c72c716ac8ba37f73e41
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-10-09 22:53:34 +0530

    Create Delete Last Node Of Doubly Linked List.cpp

---
+++ Delete Last Node Of Doubly Linked List.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition of doubly linked list:
+ *
+ * struct Node {
+ *      int data;
+ *      Node *prev;
+ *      Node *next;
+ *      Node() : data(0), prev(nullptr), next(nullptr) {}
+ *      Node(int val) : data(val), prev(nullptr), next(nullptr) {}
+ *      Node(int val, Node *p, Node *n) : data(val), prev(p), next(n) {}
+ * };
+ *
+ *************************************************************************/
+
+Node * deleteLastNode(Node *head) {
+    if(head==NULL || head->next==NULL) return NULL;
+    Node* temp=head;
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    Node* curr=temp->next;
+    curr->prev=NULL;
+    temp->next=NULL;
+    delete curr;
+    return head;
+}
