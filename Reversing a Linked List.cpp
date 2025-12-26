dcdf7dee11add51cfd0a403f6bacc9b2df21ea17
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-12-26 19:49:19 +0530

    Update Reversing a Linked List.cpp

--- Reversing a Linked List.cpp
+++ Reversing a Linked List.cpp
@@ -1,57 +1,49 @@
-//1st Approach
-Node* reverseLinkedList(Node *head)
-{
-    if(head==NULL || head->next==NULL)
-    {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        //Brute Force
+        stack<int> st;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            st.push(temp->val);
+        }
+        temp=head;
+        while(temp!=NULL){
+            temp->val=st.top();
+            st.pop();
+        }
         return head;
-    }
-    Node* prev=NULL;
-    Node* curr=head;
-    Node* forward=NULL;
-    while(curr!=NULL)
-    {
-        forward=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=forward;
 
-    }
-    return prev;
-}
+        //Iterative
+        ListNode* front=head;
+        ListNode* prev=NULL;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
 
-//2nd Approach
-void solve(Node* &head, Node* curr, Node* prev)
-{
-    if(curr==NULL)
-    {
-        head=prev;
-        return;
+        //Recursive
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* newHead=reverseList(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=NULL;
+        return newHead;
     }
-    Node* forward=curr->next;
-    solve(head, forward, curr);
-    curr->next=prev;
-}
-Node* reverseLinkedList(Node *head)
-{
-    Node* curr=head;
-    Node* prev=NULL;
-    solve(head, curr, prev);
-    return head;
-} 
-
-//3rd Approach
-Node* reverse1(Node* head)
-{
-    if(head == NULL || head->next == NULL)
-    {
-        return head;
-    }
-    Node* smallhead=reverse1(head->next);
-    head->next->next=head;
-    head->next=NULL;
-    return smallhead;
-}
-Node* reverseLinkedList(Node *head)
-{
-    reverse1(head);
-}
+};
