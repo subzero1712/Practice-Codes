9ba7e46bd20b9e62537151a5485a12df32bc57af
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2025-12-30 10:53:06 +0530

    Update Longest String Chain Using DP.cpp

--- Longest String Chain Using DP.cpp
+++ Longest String Chain Using DP.cpp
@@ -1,40 +1,74 @@
 class Solution {
-    public:
-    static bool comp(string &st1, string &st2){
-        return st1.size()<st2.size();
-    }
-    public:
-    bool check(string &s1, string &s2){
-        int first=0;
-        int second=0;
-        if(s1.size()!=s2.size()+1) return false;
-        while(first<s1.size()){
-            if(second<s2.size() && s1[first]==s2[second]){
-                first++;
-                second++;
-            }else{
-                first++;
-            }
-        }
-        if(first==s1.size() && second==s2.size()) return true;
-        return false;
-    }
 public:
-    int longestStrChain(vector<string>& words) {
-        sort(words.begin(), words.end(), comp);
-        int n=words.size();
-        vector<int> dp(n, 1);
-        int maxi = 1, lastIndex = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (check(words[i], words[j]) && 1 + dp[j] > dp[i]) {
-                    dp[i] = 1 + dp[j];
-                }
-            }
-            if (dp[i] > maxi) {
-                maxi = dp[i];
+    bool compare(string &longer, string &shorter) {
+        if (longer.length() != shorter.length() + 1) return false;
+
+        int i = 0, j = 0;
+        while (i < longer.length()) {
+            if (j < shorter.length() && longer[i] == shorter[j]) {
+                i++; j++;
+            } else {
+                i++;
             }
         }
-        return maxi;
+        return j == shorter.length();
+    }
+
+    //Memoization
+    int ans(int ind, int prev, vector<string>& words, vector<vector<int>>& dp) {
+        if (ind == words.size()) return 0;
+        if (dp[ind][prev + 1] != -1)
+            return dp[ind][prev + 1];
+        int not_take = ans(ind + 1, prev, words, dp);
+        int take = 0;
+        if (prev == -1 || compare(words[ind], words[prev])) {
+            take = 1 + ans(ind + 1, ind, words, dp);
+        }
+        return dp[ind][prev + 1] = max(take, not_take);
+    }
+
+    int longestStrChain(vector<string>& words) {
+        //Tabulation
+        sort(words.begin(), words.end(),
+             [](string &a, string &b){
+                 return a.length() < b.length();
+             });
+
+        int n = words.size();
+        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                int not_take=dp[i+1][j+1];
+                int take=0;
+                if(j==-1 || compare(words[i], words[j])){
+                    take=1+dp[i+1][i+1];
+                }
+                dp[i][j+1]=max(take,not_take);
+            }
+        }
+        return dp[0][0];
+        //return ans(0, -1, words, dp);
+
+        //Space optimisation
+        sort(words.begin(), words.end(),
+             [](string &a, string &b){
+                 return a.length() < b.length();
+             });
+
+        int n = words.size();
+        vector<int> ahead(n+1,0);
+        vector<int> curr(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                int not_take=ahead[j+1];
+                int take=0;
+                if(j==-1 || compare(words[i], words[j])){
+                    take=1+ahead[i+1];
+                }
+                curr[j+1]=max(take,not_take);
+            }
+            ahead=curr;
+        }
+        return ahead[0];
     }
 };
