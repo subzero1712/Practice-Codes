1b58813a8cd7c7ea93c6eabe10b649c94f15a173
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2026-01-20 23:43:13 +0530

    Update Delete Middle Node of Linked List.cpp

--- Delete Middle Node of Linked List.cpp
+++ Delete Middle Node of Linked List.cpp
@@ -1,25 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if (head == NULL || head->next == NULL) return NULL;
-        
-        int cnt = 0;
-        ListNode* temp = head;
-        
-        while (temp != NULL) {
-            temp = temp->next;
-            cnt++;
+        // if(head == NULL || head->next == NULL) return NULL;
+        // int cnt = 0;
+        // ListNode* temp = head;
+        // while(temp != NULL){
+        //     cnt++;
+        //     temp = temp->next;
+        // }
+        // int mid = cnt / 2;   // middle index (0-based)
+        // temp = head;
+        // ListNode* prev = NULL;
+        // for(int i = 0; i < mid; i++){
+        //     prev = temp;
+        //     temp = temp->next;
+        // }
+        // prev->next = temp->next;
+        // delete temp;
+        // return head;
+
+        if(head==NULL || head->next==NULL) return NULL;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        ListNode* prev=NULL;
+        while(fast!=NULL && fast->next!=NULL){
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-
-        temp = head;
-        for (int i = 0; i < cnt / 2 - 1; i++) {
-            temp = temp->next;
-        }
-
-        ListNode* del = temp->next;
-        temp->next = temp->next->next;
-        delete del;
-
+        prev->next=slow->next;
+        delete slow;
         return head;
     }
 };
