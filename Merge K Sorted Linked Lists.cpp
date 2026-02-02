76d2c0782990568472e2e7bc0adfabd7c6e198f8
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2026-02-03 02:24:45 +0530

    Update Merge K Sorted Linked Lists.cpp

--- Merge K Sorted Linked Lists.cpp
+++ Merge K Sorted Linked Lists.cpp
@@ -1,13 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+//Brute Force Solution
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+        vector<int> ans;
+        for(int i=0;i<lists.size();i++){
+            ListNode* temp=lists[i];
+            while(temp!=NULL){
+                ans.push_back(temp->val);
+                temp=temp->next;
+            }
+        }
+        sort(ans.begin(),ans.end());
+        ListNode* dummy=new ListNode(-1);
+        ListNode* dumNxt=dummy;
+        for(int i=0;i<ans.size();i++){
+            dumNxt->next=new ListNode(ans[i]);
+            dumNxt=dumNxt->next;
+        }
+        dumNxt->next=NULL;
+        return dummy->next;
+    }
+};
+
+//Better Solution
 class Solution {
 public:
     ListNode* merge(ListNode* temp1, ListNode* temp2){
@@ -39,23 +52,6 @@ public:
         return dummy->next;
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // vector<int> ans;
-        // for(int i=0;i<lists.size();i++){
-        //     ListNode* temp=lists[i];
-        //     while(temp!=NULL){
-        //         ans.push_back(temp->val);
-        //         temp=temp->next;
-        //     }
-        // }
-        // sort(ans.begin(),ans.end());
-        // ListNode* dummy=new ListNode(-1);
-        // ListNode* dumNxt=dummy;
-        // for(int i=0;i<ans.size();i++){
-        //     dumNxt->next=new ListNode(ans[i]);
-        //     dumNxt=dumNxt->next;
-        // }
-        // dumNxt->next=NULL;
-        // return dummy->next;
         if(lists.size()==0) return NULL;
         ListNode* head=lists[0];
         for(int i=1;i<lists.size();i++){
@@ -64,3 +60,24 @@ public:
         return head;
     }
 };
+
+//Optimal Solution
+ListNode* mergeKLists(vector<ListNode*>& lists){
+        priority_queue<pair<int,ListNode*>, 
+        vector<pair<int,ListNode*>>, greater<pair<int,ListNode*>>> pq;
+        ListNode* dummy=new ListNode(-1);
+        ListNode* dum=dummy;
+        for(int i=0;i<lists.size();i++){
+            ListNode* node=lists[i];
+            if(node!=NULL) pq.push({node->val, node});
+        }
+        while(!pq.empty()){
+            auto top=pq.top();
+            pq.pop();
+            ListNode* nodes=top.second;
+            dum->next=nodes;
+            if(nodes->next!=NULL) pq.push({nodes->next->val,nodes->next});
+            dum=dum->next;
+        }
+        return dummy->next;
+    }
