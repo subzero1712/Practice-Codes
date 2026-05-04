40ac10c2c0e0405c915625df3b33e905d5143ec5
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2026-05-04 19:27:32 +0530

    Update Check If Kth Bit From Right is Set or Not.cpp

--- Check If Kth Bit From Right is Set or Not.cpp
+++ Check If Kth Bit From Right is Set or Not.cpp
@@ -1,3 +1,4 @@
+//Using Left Shift Operator
 bool isKthBitSet(int n, int k)
 {
     if((n&(1<<k-1))!=0){
@@ -6,4 +7,13 @@ bool isKthBitSet(int n, int k)
     else{
         return false;
     }
+}
+
+//Using Right Shift Operator
+bool isKthBitSet(int n, int k)
+{
+    if((n>>k-1)&1==1){
+        return true;
+    }
+    else return false;
 }
