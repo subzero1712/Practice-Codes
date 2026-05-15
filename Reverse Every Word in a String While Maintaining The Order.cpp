85422872789e7797b7c2711dac822208a17c3853
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2026-05-16 00:38:23 +0530

    Create Reverse Every Word in a String While Maintaining The Order.cpp

---
+++ Reverse Every Word in a String While Maintaining The Order.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans="";
+        int n=s.length();
+        for(int i=0;i<s.length();i++){
+            string word="";
+            while(i<n && s[i]!=' '){
+                word+=s[i];
+                i++;
+            }
+            reverse(word.begin(), word.end());
+            ans+=" "+word;
+        }
+        return ans.substr(1);
+    }
+};
