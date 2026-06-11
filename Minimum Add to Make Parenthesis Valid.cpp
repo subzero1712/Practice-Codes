7495d2aa61dbf4024468aec6817387b4cf0842a2
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2026-06-11 11:21:18 +0530

    Create Minimum Add to Make Parenthesis Valid.cpp

---
+++ Minimum Add to Make Parenthesis Valid.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int size=0;
+        int open=0;
+        for(auto ch:s){
+            if(ch=='('){
+                size++;
+            }else if(size>0){
+                size--;
+            }else{
+                open++;
+            }
+        }
+        return size+open;
+    }
+};
