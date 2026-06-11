4aedddc1f7ed3243c40d4e5857cea7b8a5172f1a
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2026-06-11 11:54:30 +0530

    Create Count And Say.cpp

---
+++ Count And Say.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n==1){
+            return "1";
+        }
+        string say=countAndSay(n-1);
+
+        string result="";
+        for(int i=0;i<say.length();i++){
+            int cnt=1;
+            char ch=say[i];
+            while(i<say.length()-1 && say[i]==say[i+1]){
+                cnt++;
+                i++;
+            }
+            result+=to_string(cnt)+string(1,ch);
+        }
+        return result;
+    }
+};
