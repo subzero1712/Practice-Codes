fd89c9e8e22def81232641bd8ba1ab2da9470d61
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2026-06-11 13:15:44 +0530

    Create Repeated String Match Using Rabin Karp Algorithm (Rolling Hash).cpp

---
+++ Repeated String Match Using Rabin Karp Algorithm (Rolling Hash).cpp
@@ -0,0 +1,68 @@
+//Better Approach
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string ans="";
+        int cnt=0;
+        while(ans.size()<b.size()){
+            ans+=a;
+            cnt++;
+        }
+        if(ans.find(b)!=string::npos){
+            return cnt;
+        }
+        ans+=a;
+        cnt++;
+        if(ans.find(b)!=string::npos){
+            return cnt;
+        }
+        return -1;
+    }
+};
+
+//Rabin Karp Algorithm Optimal Approach
+class Solution {
+private:
+    int BASE = 1000000;
+public:
+    int repeatedStringMatch(string A, string B) {
+        if(A == B) return 1;
+        int count = 1;
+        string source = A;
+        while(source.size() < B.size()){
+            count++;
+            source+=A;
+        }
+        if(source == B) return count;
+        if(Rabin_Karp(source,B) != -1) return count;
+        if(Rabin_Karp(source+A,B) != -1) return count+1;
+        return -1;
+    }
+    int Rabin_Karp(string source, string target){
+        if(source == "" or target == "") return -1;
+        int m = target.size();
+        int power = 1;
+        for(int i = 0;i<m;i++){
+            power = (power*31)%BASE;
+        }
+        int targetCode = 0;
+        for(int i = 0;i<m;i++){
+            targetCode = (targetCode*31+target[i])%BASE;
+        }
+        int hashCode = 0;
+        for(int i = 0;i<source.size();i++){
+            hashCode = (hashCode*31 + source[i])%BASE;
+            if(i<m-1) continue;
+            if(i>=m){
+                hashCode = (hashCode-source[i-m]*power)%BASE;
+            }
+            if(hashCode<0)
+                hashCode+=BASE;
+            if(hashCode == targetCode){
+                if(source.substr(i-m+1,m) == target)
+                    return i-m+1;
+            }
+        }
+        return -1;
+    }
+};
