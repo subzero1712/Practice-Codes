e2dc17fdc9c838e706f6e2b0225ee1c0d397060b
subzero1712 <136018915+subzero1712@users.noreply.github.com>
2026-06-29 12:05:40 +0530

    Update Convert String to Integer.cpp

--- Convert String to Integer.cpp
+++ Convert String to Integer.cpp
@@ -1,3 +1,4 @@
+//Non Recursive
 class Solution {
 public:
     int myAtoi(string s) {
@@ -28,5 +29,44 @@ public:
             i++;
         }
         return num*sign;
+    }
+};
+
+//Recursive
+class Solution {
+public:
+
+    int solve(string &s, int i, long long num, int sign) {
+
+        if (i >= s.size() || !isdigit(s[i]))
+            return num * sign;
+
+        num = num * 10 + (s[i] - '0');
+
+        if (num * sign > INT_MAX)
+            return INT_MAX;
+
+        if (num * sign < INT_MIN)
+            return INT_MIN;
+
+        return solve(s, i + 1, num, sign);
+    }
+
+    int myAtoi(string s) {
+
+        int i = 0;
+        int sign = 1;
+        int n = s.size();
+
+        while (i < n && s[i] == ' ')
+            i++;
+
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
+            i++;
+        }
+
+        return solve(s, i, 0, sign);
     }
 };
